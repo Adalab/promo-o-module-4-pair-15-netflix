d52fe085c4d8401fe4cffbce6f3d677343fee652
Your Name <you@example.com>
2022-02-02 11:32:20 +0100

    2ndDay

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const movies = require('./data/movies.json')
 
 // create and config server
 const server = express();
@@ -11,3 +12,13 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+server.get("/movies", (req, res) => {
+  console.log('Peticion a la ruta GET /movies')
+  
+  movies = {
+    success: true,
+    
+  }
+  res.json(movies)
+})
--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -3,9 +3,14 @@
 const getMoviesFromApi = () => {
   console.log('Se están pidiendo las películas de la app');
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
-    .then(() => {
+  return fetch('http://localhost:4000/movies', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+     return data;
+  });
+    /*.then(() => {
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return {
         success: true,
@@ -26,7 +31,7 @@ const getMoviesFromApi = () => {
           }
         ]
       };
-    });
+    });*/
 };
 
 const objToExport = {
