29add540342b23a1fb22c87573f70f5c6e6d4010
Natalia <atvnataliajn@gmail.com>
2022-02-08 11:31:01 +0100

    Day 6: add sign up

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const Database = require("better-sqlite3");
 const users = require("./data/users.json");
-const movies = require('./data/movies.json')
+const movies = require("./data/movies.json");
 
 // create and config server
 const server = express();
@@ -27,8 +27,8 @@ server.listen(serverPort, () => {
 //Y crear motor de plantillas
 server.get("/movie/:movieId", (req, res) => {
   console.log(req.params.movieId);
-  const query = db.prepare("SELECT * FROM movies WHERE id = ?")
-  const idMovies = query.get(req.params.movieId)
+  const query = db.prepare("SELECT * FROM movies WHERE id = ?");
+  const idMovies = query.get(req.params.movieId);
 
   res.render("movie", idMovies);
 });
@@ -37,16 +37,13 @@ server.get("/movies", (req, res) => {
   console.log("Peticion a la ruta GET /movies");
   console.log(req.query.gender);
   const gender = req.query.gender;
-  
+
   const query = db.prepare("SELECT * FROM movies WHERE gender = ?");
   const allMovies = query.all(gender);
- 
 
- 
   const response = {
     success: true,
     movies: req.query.gender === "" ? movies : allMovies,
-  
   };
 
   res.json(response);
@@ -56,12 +53,11 @@ server.post("/login", (req, res) => {
   console.log(req.body);
   console.log("Peticion a la ruta LOGIN");
   const email = req.body.email;
-  const password =req.body.password
-  const query = db.prepare("SELECT * FROM users WHERE email = ? and password = ?");
+  const password = req.body.password;
+  const query = db.prepare(
+    "SELECT * FROM users WHERE email = ? and password = ?"
+  );
   const getUser = query.get(email, password);
-  /*const foundUser = users.find((user) => {
-    return user.email === req.body.email && user.password === req.body.password;
-  });*/
 
   const successfully = {
     success: true,
@@ -77,6 +73,32 @@ server.post("/login", (req, res) => {
     res.json(successfully);
   } else {
     res.json(unsuccessfully);
+  }
+});
+
+// Registrar nueva usuaria
+server.post("/signup", (req, res) => {
+  console.log(req.body);
+  console.log("Petición a la ruta signup");
+  const email = req.body.email;
+  const password = req.body.password;
+  const selectUser = db.prepare("SELECT * FROM users WHERE email = ?");
+  const foundUser = selectUser.get(email);
+
+  if (foundUser === undefined) {
+    const query = db.prepare(
+      "INSERT INTO users (email, password) VALUES (?, ?)"
+    );
+    const addUser = query.run(email, password);
+    res.json({
+      success: true,
+      userId: addUser.lastInsertRowid,
+    });
+  } else{
+    res.json({
+      success: false,
+      message: "el usuario ya existe",
+    })
   }
 });
 
